5d7790ad7233a06c62824be6650edc378a12f9fc
Ravi Ranjan <ranjan21061@gmail.com>
2025-11-27 15:37:41 +0530

    In home page login button has been added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,9 @@ export default function Home() {
   return (
     <>
       <h2 style={{ textAlign: "center" }}>Welcome to rasChat Application New!</h2>
+      <a href="/login" className="btn btn-primary px-4 py-2">
+        Login Here
+      </a>
     </>
   );
 }
